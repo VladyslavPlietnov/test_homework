b443457595429c93c735e36c993019e9ea9d149b
Plietnov Vladyslav <pletniov@ukr.net>
2023-10-18 14:18:32 +0200

    second commit

--- src/main/java/org/example/SumCalulator.java
+++ src/main/java/org/example/SumCalulator.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class SumCalulator {
+    public int sum(int a) throws IllegalArgumentException{
+        if(a <1){
+            throw new IllegalArgumentException("value cannot be 0 or less");
+        }
+        {
+            int i = 1;
+            int result = 0;
+            while (i <= a) {
+                result += i;
+                i++;
+            }
+            return result;
+        }
     }
+
 }
--- src/test/java/org/example/SumCalulatorTest.java
+++ src/test/java/org/example/SumCalulatorTest.java
@@ -1,4 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SumCalulatorTest {
-  
+    SumCalulator calculator;
+    int[] input = {1,3};
+    int[] output = {1,6};
+    @BeforeEach
+    public void beforeEach(){
+        calculator = new SumCalulator();
+    }
+    @Test
+    public void sumTest(){
+      for(int i= 0; i<input.length; i++){
+         Assertions.assertEquals(output[i],calculator.sum(input[i]));
+      }
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> calculator.sum(0));
+    }
+
 }
